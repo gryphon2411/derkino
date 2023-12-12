6af055eeb6b486ca62d2e537a5b01ec4df510438
Eido Askayo <eido.askayo@gmail.com>
2023-12-12 18:36:17 +0200

    Revert "Implement next.js ui titles page"
    This reverts commit 9cf2d57fe772faea3873d89026fa365d6fcde835.

--- uis/react-ui/derkino-ui/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import titlesReducer from '@/redux/titlesSlice';
-
-export default configureStore({
-  reducer: {
-    titles: titlesReducer,
-  },
-});
