201e9eb012bb6d8aeb3b71bcc385669d3ecd1f1f
Eido Askayo <eido.askayo@gmail.com>
2023-12-12 13:07:00 +0200

    Add next.js titles table pagination

--- uis/react-ui/derkino-ui/src/hooks/useTitles.js
+++ uis/react-ui/derkino-ui/src/hooks/useTitles.js
@@ -1,13 +1,21 @@
 import useSWR from 'swr'
+import { useState } from 'react';
 
 const fetcher = url => fetch(url).then(res => res.json());
 
 export default function useTitles() {
-  const { data, error } = useSWR('http://192.168.49.2:32062/api/v1/titles?page=0&size=2', fetcher);
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(10);
+
+  const { data, error } = useSWR(`http://192.168.49.2:32062/api/v1/titles?page=${page}&size=${size}`, fetcher);
 
   return {
     titles: data ? data.content : null,
     isLoading: !error && !data,
-    isError: error
+    isError: error,
+    setPage,
+    setSize,
+    size,
+    page
   }
 }
