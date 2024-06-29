4a5e5813ad8dbb3994c287a36b43a12802315ec7
Eido Askayo <11075654+gryphon2411@users.noreply.github.com>
2024-06-29 19:36:26 +0300

    Add spring auth remember me

--- services/spring-boot/auth_service/src/main/java/com/derkino/auth_service/AuthServiceSecurityConfig.java
+++ services/spring-boot/auth_service/src/main/java/com/derkino/auth_service/AuthServiceSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -29,7 +30,9 @@ public class AuthServiceSecurityConfig {
                 .formLogin(formLogin ->
                         formLogin
                                 .loginPage("/login")
-                                .permitAll());
+                                .permitAll())
+
+                .rememberMe(Customizer.withDefaults());
 
         return http.build();
     }
