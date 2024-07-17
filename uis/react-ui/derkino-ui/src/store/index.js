7d055ce7100997638f0bfda74872b8cc7b731118
Eido Askayo <11075654+gryphon2411@users.noreply.github.com>
2024-07-17 15:46:51 +0300

    Revert "Revert "Fix derkino ui build errors""
    This reverts commit 829c4610b0c595213619812fab66a5c88b61c5cb.

--- uis/react-ui/derkino-ui/src/store/index.js
+++ uis/react-ui/derkino-ui/src/store/index.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import appReducer from '@/app/slice'
 import titlesReducer from '@/app/titles/slice';
 import titleReducer from '@/app/titles/[id]/slice'
-import loginReducer from '@/pages/login/slice';
+import loginReducer from '@/pages-slices/login/slice';
 
 export const store = configureStore({
   reducer: {
