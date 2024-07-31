17aec890d547350fda9c17da02daaf4532573fa8
Eido Askayo <11075654+gryphon2411@users.noreply.github.com>
2024-07-31 08:45:19 +0300

    Fix generative service title facts url

--- services/django-rest-framework/generative_service/generative_service_app/urls.py
+++ services/django-rest-framework/generative_service/generative_service_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from generative_service_app import views
 
 urlpatterns = [
-    path('title/<int:title_id>/facts', views.TitleFacts.as_view())
+    path('title/<str:title_id>/facts', views.TitleFacts.as_view())
 ]
