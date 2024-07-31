472873010ff057c653f576d768f2117e73b11b08
Eido Askayo <11075654+gryphon2411@users.noreply.github.com>
2024-07-31 11:56:49 +0300

    Fix data service missing security annotations

--- services/spring-boot/data_service/src/main/java/com/derkino/data_service/DataServiceSecurityConfig.java
+++ services/spring-boot/data_service/src/main/java/com/derkino/data_service/DataServiceSecurityConfig.java
@@ -2,8 +2,10 @@ package com.derkino.data_service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,6 +14,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+@Configuration
+@EnableWebSecurity
 public class DataServiceSecurityConfig {
     @Value("${derkino.server.prefix-path}")
     private String serverPrefixPath;
