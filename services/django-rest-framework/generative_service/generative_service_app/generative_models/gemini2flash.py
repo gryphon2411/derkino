235e443bfd1d1d9432704eec39b854dbc0f75293
Eido Askayo <11075654+gryphon2411@users.noreply.github.com>
2025-02-07 17:10:21 +0200

    Improve generative service gemini 2 flash model
    - Added system instruction.

--- services/django-rest-framework/generative_service/generative_service_app/generative_models/gemini2flash.py
+++ services/django-rest-framework/generative_service/generative_service_app/generative_models/gemini2flash.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 from django.apps import apps
 from google import genai
-
+from google.genai import types
 
 gemini2flash_generative_model = None  # type: Optional[Gemini2FlashGenerativeModel]
 
@@ -20,13 +20,15 @@ def get_gemini2flash_generative_model():
 class Gemini2FlashGenerativeModel:
     MODEL_NAME = "gemini-2.0-flash"
     GEMINI_API_KEY = apps.get_app_config("generative_service_app").gemini_api_key
-    TITLE_FACTS_PROMPT_TEMPLATE = ("List, as raw text, 3 interesting facts "
-                                   "about \"{title_name}\" ({title_year}) {title_type}.")
+    TITLE_FACTS_SYSTEM_INSTRUCTION = ("When providing lists of facts or information, present the facts directly "
+                                      "without introductory or concluding sentences.")
+    TITLE_FACTS_PROMPT_TEMPLATE = "List 3 interesting facts about \"{title_name}\" ({title_year}) {title_type}."
 
     def __init__(self):
         self._logger = getLogger(self.__class__.__name__)
         self._model_name = self.MODEL_NAME
         self._client = genai.Client(api_key=self.GEMINI_API_KEY)
+        self._content_config=types.GenerateContentConfig(system_instruction=self.TITLE_FACTS_SYSTEM_INSTRUCTION)
 
     def prompt_title_facts(self, title_name, title_year, title_type):
         input_text = self.TITLE_FACTS_PROMPT_TEMPLATE.format(title_name=title_name, title_year=title_year,
@@ -36,6 +38,7 @@ class Gemini2FlashGenerativeModel:
 
         self._logger.warning(f"Generating '{self._model_name}' outputs for title facts prompt")
 
-        model_response = self._client.models.generate_content(model=self._model_name, contents=contents)
+        model_response = self._client.models.generate_content(model=self._model_name, config=self._content_config,
+                                                              contents=contents)
 
         return model_response.text
